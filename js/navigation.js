ee0387aa0be7ce8b97b83a13b90d8c65891dbb76
widiopranoto <widiopranoto@gmail.com>
2019-05-07 16:20:54 +0700

    first commit

---
+++ js/navigation.js
@@ -0,0 +1,199 @@
+function writeSidebar() {
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_1');return false;">Lesson 1</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_2');return false;">Lesson 2</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_3');return false;">Lesson 3</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_4');return false;">Lesson 4</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_5');return false;">Lesson 5</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_6');return false;">Lesson 6</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_7');return false;">Lesson 7</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_8');return false;">Lesson 8</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_9');return false;">Lesson 9</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_10');return false;">Lesson 10</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_11');return false;">Lesson 11</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_12');return false;">Lesson 12</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_13');return false;">Lesson 13</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_14');return false;">Lesson 14</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_15');return false;">Lesson 15</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_16');return false;">Lesson 16</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_17');return false;">Lesson 17</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_18');return false;">Lesson 18</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_19');return false;">Lesson 19</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_20');return false;">Lesson 20</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_21');return false;">Lesson 21</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_22');return false;">Lesson 22</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_23');return false;">Lesson 23</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_24');return false;">Lesson 24</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_25');return false;">Lesson 25</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_26');return false;">Lesson 26</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_27');return false;">Lesson 27</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_28');return false;">Lesson 28</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_29');return false;">Lesson 29</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_30');return false;">Lesson 30</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_31');return false;">Lesson 31</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_32');return false;">Lesson 32</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_33');return false;">Lesson 33</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_34');return false;">Lesson 34</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_35');return false;">Lesson 35</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_36');return false;">Lesson 36</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_37');return false;">Lesson 37</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_38');return false;">Lesson 38</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_39');return false;">Lesson 39</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_40');return false;">Lesson 40</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_41');return false;">Lesson 41</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_42');return false;">Lesson 42</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_43');return false;">Lesson 43</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_44');return false;">Lesson 44</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_45');return false;">Lesson 45</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_46');return false;">Lesson 46</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_47');return false;">Lesson 47</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_48');return false;">Lesson 48</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_49');return false;">Lesson 49</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_50');return false;">Lesson 50</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_51');return false;">Lesson 51</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_52');return false;">Lesson 52</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_53');return false;">Lesson 53</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_54');return false;">Lesson 54</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_55');return false;">Lesson 55</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_56');return false;">Lesson 56</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_57');return false;">Lesson 57</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_58');return false;">Lesson 58</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_59');return false;">Lesson 59</a>`);
+    document.write(`<a href="" class="list-group-item list-group-item-success" onclick="lessonAppear('#Lesson_60');return false;">Lesson 60</a>`);
+}
+
+function writeLesson(lesson, title, instruction, level, mp3, pic1, pic2, pic3, pic4) {
+    var instruction = "Listen to the recording, read the text and do the exercise!";
+    document.write(`<div id="` + lesson + `" class="container-fluid col-sm-8">`);
+    document.write(`<h3>` + lesson + `</h3>`);
+    document.write(`<h3>` + title + `</h3>`);
+    document.write(`<p>Instruction:</p>`);
+    document.write(`<p>` + instruction + `</p>`);
+    document.write(`<audio controls>`);
+    document.write(`<source src="` + level + `/audio/` + mp3 + `" type="audio/mpeg">`);
+    document.write(`Your browser does not support the audio element.`);
+    document.write(`</audio>`);
+    document.write(`<img src="` + level + `/image/` + pic1 + `" class="img-rounded" height="100%" width="100%">`);
+    document.write(`<img src="` + level + `/image/` + pic2 + `" class="img-rounded" height="100%" width="100%">`);
+    document.write(`<img src="` + level + `/image/` + pic3 + `" class="img-rounded" height="100%" width="100%">`);
+    document.write(`<img src="` + level + `/image/` + pic4 + `" class="img-rounded" height="100%" width="100%">`);
+    document.write(`</div>`);
+}
+
+function writeListening(lesson, title, instruction, level, mp3, pic1, pic2, pic3, pic4) {
+    document.write(`<div id="` + lesson + `" class="container-fluid col-sm-8">`);
+    document.write(`<h3>` + lesson + `</h3>`);
+    document.write(`<h3>` + title + `</h3>`);
+    document.write(`<p>Instruction:</p>`);
+    document.write(`<p>` + instruction + `</p>`);
+    document.write(`<audio controls>`);
+    document.write(`<source src="` + level + `/audio/` + mp3 + `" type="audio/mpeg">`);
+    document.write(`Your browser does not support the audio element.`);
+    document.write(`</audio>`);
+    document.write(`<img src="` + level + `/image/` + pic1 + `" class="img-rounded">`);
+    document.write(`<img src="` + level + `/image/` + pic2 + `" class="img-rounded">`);
+    document.write(`<img src="` + level + `/image/` + pic3 + `" class="img-rounded">`);
+    document.write(`<img src="` + level + `/image/` + pic4 + `" class="img-rounded">`);
+    document.write(`</div>`);
+}
+
+function writeSpeaking(lesson, title, instruction, level, mp3, pic1, pic2, pic3, pic4) {
+    document.write(`<div id="` + lesson + `" class="container-fluid col-sm-8">`);
+    document.write(`<h3>` + lesson + `</h3>`);
+    document.write(`<h3>` + title + `</h3>`);
+    document.write(`<p>Instruction:</p>`);
+    document.write(`<p>` + instruction + `</p>`);
+    document.write(`<audio controls>`);
+    document.write(`<source src="` + level + `/audio/` + mp3 + `" type="audio/mpeg">`);
+    document.write(`Your browser does not support the audio element.`);
+    document.write(`</audio>`);
+    document.write(`<img src="` + level + `/image/` + pic1 + `" class="img-rounded">`);
+    document.write(`<img src="` + level + `/image/` + pic2 + `" class="img-rounded">`);
+    document.write(`<img src="` + level + `/image/` + pic3 + `" class="img-rounded">`);
+    document.write(`<img src="` + level + `/image/` + pic4 + `" class="img-rounded">`);
+    document.write(`</div>`);
+}
+
+function writeSubNav() {
+    document.write("<div class='subnav'>");
+    document.write("<button class='subnavbtn'>Schedule<i class='fa fa-caret-down'></i></button><div class='subnav-content'><a href='#'>Show My Schedule</a><a href='#'>Edit My Schedule</a></div></div>");
+}
+
+function writeNavHome() {
+    document.write("<div class='navbar'>");
+    document.write("<a class='active' href='index.html'><i class='fa fa-fw fa-home'></i> Home</a>");
+    document.write("<a href='schedule.html'><i class='fa fa-fw fa-calendar'></i> Schedule</a>");
+    document.write("<a href='journal.html'><i class='fa fa-fw fa-book'></i> Journal</a>");
+    document.write("<a href='attendant_list.html'><i class='fa fa-fw fa-file'></i> Attendant List</a>");
+    document.write("<a href='grades.html'><i class='fa fa-fw fa-file-alt'></i> Grades</a>");
+    document.write("<a href='teacher_briefcase.html'><i class='fa fa-fw fa-briefcase'></i> Teacher's Briefcase</a>");
+    document.write("<a href='./LatihanUN/index.html'><i class='fa fa-fw fa-sign-pencil'></i> Latihan UN</a>");
+    document.write("<a href='login.html'><i class='fa fa-fw fa-sign-in'></i> Login</a>");
+    document.write("<a href='logout.html'><i class='fa fa-fw fa-sign-out'></i> Logout</a>");
+    document.write("</div>");
+}
+function writeNavSchedule() {
+    document.write("<div class='navbar'>");
+    document.write("<a href='index.html'><i class='fa fa-fw fa-home'></i> Home</a>");
+    document.write("<a class='active'href='schedule.html'><i class='fa fa-fw fa-calendar'></i> Schedule</a>");
+    document.write("<a href='journal.html'><i class='fa fa-fw fa-book'></i> Journal</a>");
+    document.write("<a href='attendant_list.html'><i class='fa fa-fw fa-file'></i> Attendant List</a>");
+    document.write("<a href='grades.html'><i class='fa fa-fw fa-file-alt'></i> Grades</a>");
+    document.write("<a href='teacher_briefcase.html'><i class='fa fa-fw fa-briefcase'></i> Teacher's Briefcase</a>");
+    document.write("<a href='./LatihanUN/index.html'><i class='fa fa-fw fa-sign-pencil'></i> Latihan UN</a>");
+    document.write("<a href='login.html'><i class='fa fa-fw fa-sign-in'></i> Login</a>");
+    document.write("<a href='logout.html'><i class='fa fa-fw fa-sign-out'></i> Logout</a>");
+    document.write("</div>");
+}
+function writeNavJournal() {
+    document.write("<div class='navbar'>");
+    document.write("<a href='index.html'><i class='fa fa-fw fa-home'></i> Home</a>");
+    document.write("<a href='schedule.html'><i class='fa fa-fw fa-calendar'></i> Schedule</a>");
+    document.write("<a class='active'href='journal.html'><i class='fa fa-fw fa-book'></i> Journal</a>");
+    document.write("<a href='attendant_list.html'><i class='fa fa-fw fa-file'></i> Attendant List</a>");
+    document.write("<a href='grades.html'><i class='fa fa-fw fa-file-alt'></i> Grades</a>");
+    document.write("<a href='teacher_briefcase.html'><i class='fa fa-fw fa-briefcase'></i> Teacher's Briefcase</a>");
+    document.write("<a href='./LatihanUN/index.html'><i class='fa fa-fw fa-sign-pencil'></i> Latihan UN</a>");
+    document.write("<a href='login.html'><i class='fa fa-fw fa-sign-in'></i> Login</a>");
+    document.write("<a href='logout.html'><i class='fa fa-fw fa-sign-out'></i> Logout</a>");
+    document.write("</div>");
+}
+function writeNavAttendant() {
+    document.write("<div class='navbar'>");
+    document.write("<a href='index.html'><i class='fa fa-fw fa-home'></i> Home</a>");
+    document.write("<a href='schedule.html'><i class='fa fa-fw fa-calendar'></i> Schedule</a>");
+    document.write("<a href='journal.html'><i class='fa fa-fw fa-book'></i> Journal</a>");
+    document.write("<a class='active'href='attendant_list.html'><i class='fa fa-fw fa-file'></i> Attendant List</a>");
+    document.write("<a href='grades.html'><i class='fa fa-fw fa-file-alt'></i> Grades</a>");
+    document.write("<a href='teacher_briefcase.html'><i class='fa fa-fw fa-briefcase'></i> Teacher's Briefcase</a>");
+    document.write("<a href='./LatihanUN/index.html'><i class='fa fa-fw fa-sign-pencil'></i> Latihan UN</a>");
+    document.write("<a href='login.html'><i class='fa fa-fw fa-sign-in'></i> Login</a>");
+    document.write("<a href='logout.html'><i class='fa fa-fw fa-sign-out'></i> Logout</a>");
+    document.write("</div>");
+}
+function writeNavGrades() {
+    document.write("<div class='navbar'>");
+    document.write("<a href='index.html'><i class='fa fa-fw fa-home'></i> Home</a>");
+    document.write("<a href='schedule.html'><i class='fa fa-fw fa-calendar'></i> Schedule</a>");
+    document.write("<a href='journal.html'><i class='fa fa-fw fa-book'></i> Journal</a>");
+    document.write("<a href='attendant_list.html'><i class='fa fa-fw fa-file'></i> Attendant List</a>");
+    document.write("<a class='active' href='grades.html'><i class='fa fa-fw fa-file-alt'></i> Grades</a>");
+    document.write("<a href='teacher_briefcase.html'><i class='fa fa-fw fa-briefcase'></i> Teacher's Briefcase</a>");
+    document.write("<a href='./LatihanUN/index.html'><i class='fa fa-fw fa-sign-pencil'></i> Latihan UN</a>");
+    document.write("<a href='login.html'><i class='fa fa-fw fa-sign-in'></i> Login</a>");
+    document.write("<a href='logout.html'><i class='fa fa-fw fa-sign-out'></i> Logout</a>");
+    document.write("</div>");
+}
+function writeNavTeacherBriefcase() {
+    document.write("<div class='navbar'>");
+    document.write("<a href='index.html'><i class='fa fa-fw fa-home'></i> Home</a>");
+    document.write("<a href='schedule.html'><i class='fa fa-fw fa-calendar'></i> Schedule</a>");
+    document.write("<a href='journal.html'><i class='fa fa-fw fa-book'></i> Journal</a>");
+    document.write("<a href='attendant_list.html'><i class='fa fa-fw fa-file'></i> Attendant List</a>");
+    document.write("<a href='grades.html'><i class='fa fa-fw fa-file-alt'></i> Grades</a>");
+    document.write("<a class='active' href='teacher_briefcase.html'><i class='fa fa-fw fa-briefcase'></i> Teacher's Briefcase</a>");
+    document.write("<a href='./LatihanUN/index.html'><i class='fa fa-fw fa-sign-pencil'></i> Latihan UN</a>");
+    document.write("<a href='login.html'><i class='fa fa-fw fa-sign-in'></i> Login</a>");
+    document.write("<a href='logout.html'><i class='fa fa-fw fa-sign-out'></i> Logout</a>");
+    document.write("</div>");
+}
+
